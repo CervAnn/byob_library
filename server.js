00f083ed6dd8be4d8c3ecd72af9e0a86c161f662
Ann Cerveny <cervann11@gmail.com>
2019-10-08 23:04:06 -0600

    Establish 2 GET endpoints for patrons table

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +13,32 @@ app.locals.title = 'Library Patron Data';
 
 app.get('/', (request, response) => {
   response.send('Loading Library Patrons');
+});
+
+app.get('/api/v1/patrons', (request, response) => {
+  database('patrons').select()
+    .then(patrons => {
+      response.status(200).json(patrons);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/patrons/:id', (request, response) => {
+  database('patrons').where('id', request.params.id).select()
+    .then(patrons => {
+      if (patrons.length) {
+        response.status(200).json(patrons);
+      } else {
+        response.status(404).json({ 
+          error: `Could not find patron based on the id provided (${request.params.id})`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
